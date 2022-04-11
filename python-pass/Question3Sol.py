ec16f368b100f6904e5b7d0cdd942e5b7d8f967c
ahmedlord20 <103456191+ahmedlord20@users.noreply.github.com>
2022-04-11 22:55:12 +0300

    Create Question3Sol.py

---
+++ python-pass/Question3Sol.py
@@ -0,0 +1,33 @@
+# Q3
+#three input only
+print("Enter a number: 3")
+N1 = int(input());
+N2 = int(input());
+N3 = int(input());
+
+
+if (N1 % 2) == 0:
+    print("{0} is Even".format(N1))
+else:
+    print("{0} is Odd".format(N1));
+    
+    
+if (N2 % 2) == 0:
+    print("{0} is Even".format(N2))
+else:
+    print("{0} is Odd".format(N2));
+    
+    
+if (N3 % 2) == 0:
+    print("{0} is Even".format(N3))
+else:
+    print("{0} is Odd".format(N3))
+
+#one input only
+print("Enter X value: 1")
+num = int(input())
+
+if (num % 2) == 0:
+   print("{0} is Even".format(num))
+else:
+   print("{0} is Odd".format(num))
